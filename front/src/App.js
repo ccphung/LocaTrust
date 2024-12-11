9fe48e6f4f3998db6f00b4252b752b8fe3c38cf0
Cédric Phung <cedric3011@outlook.fr>
2024-12-11 17:57:26 +0100

    create routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Inscription from './pages/Inscription';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/inscription" element={<Inscription/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login/>} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ front/src/components/buttons/ButtonFilled.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ButtonFilled() {
+  return (
+    <div>ButtonFilled</div>
+  )
+}
+
+export default ButtonFilled
