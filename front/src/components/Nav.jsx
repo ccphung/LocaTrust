23a4104450f2088f2f74ca7ac34a842d052d4571
Cédric Phung <cedric3011@outlook.fr>
2024-12-14 13:36:15 +0100

    Create navbar

---
+++ front/src/components/Nav.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 25px;
+
+  @media(max-width: 992px) {
+    margin: 10px;
+  }
+`;
+
+const LogoContainer = styled.a`
+  visibility: ${(props) => (props.visibility)};
+  font-size: 2.5em;
+  cursor: pointer;
+  text-decoration: none;
+`
+const LogoLoca = styled.span`
+  color: black;
+`
+const LogoTrust = styled.span`
+  color: #520058;
+  font-weight: bold;
+`
+const NavMenu = styled.div`
+  color: #520058;
+  cursor: pointer;
+  text-shadow: 0px 5px 10px gray;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+function Nav() {
+  const homeUrl = useLocation().pathname;
+  return (
+    <NavContainer>
+      <LogoContainer href="/" visibility={`${homeUrl === "/" ? "hidden" : "visibile"}`}>
+          <LogoLoca>Loca</LogoLoca><LogoTrust>Trust</LogoTrust>
+      </LogoContainer>
+      <NavMenu>Connexion/Inscription</NavMenu>
+    </NavContainer>
+  )
+}
+
+export default Nav
