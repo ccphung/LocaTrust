9bf1b46e6cfd994fb48b4a2a91a34e5c2bb2fea7
Cédric Phung <cedric3011@outlook.fr>
2024-12-20 16:37:30 +0100

    Add carousel

--- front/src/pages/Ad.jsx
+++ front/src/pages/Ad.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import styled from 'styled-components'
-import Checkbox from '../components/Checkbox'
 import Button from '../components/Button'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 const AdContainer = styled.div`
   display:flex;
@@ -22,54 +24,61 @@ const AdWrapper = styled.div`
 const AdDetail = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 20px;
   justify-content: center;
-`
-
-const AdImg = styled.img`
-    width: 50%;
-    height: auto;
-    object-fit: cover;
 `
 
 const AdFeatures = styled.div`
   margin-left: 50px;
 `
 
-const AdFeaturesTitle = styled.div`
+const AdFeaturesText = styled.p`
+  font-size: ${(props) => props.fs};
+  font-weight: ${(props) => props.fw};
+`
 
+const AdImg = styled.img`
+  width: 80%;
+  max-width: 500px;
+  height: auto; 
+  object-fit: cover; 
+  border-radius: 10px;
 `
 
 const Ad = () => {
   return (
     <>
-      <Nav />
+        <Nav />
         <AdContainer>
           <AdWrapper>
-            <AdImg src="assets/images/ad/appartment.jpg"/>
-            <AdDetail>
-              <AdFeatures>
-                  <p>1500€/mois</p>
-                  <p>Appartement - Paris 16ème</p>
-                  <p>5 pièces - 75m² - Ascenseur</p>
-
-                  <h3>Caractéristiques</h3>
-                  <p>Jardin</p>
-                  <p>Gardien</p>
-
-                  <h3>Conditions de location</h3>
-                  <p>Minimum 6 mois</p>
-
-                  <h3>Disponibilité</h3>
-                  <p>Immédiate</p>
-
-                  <p>Note : 4.5/5</p>
-
-              </AdFeatures>
-            </AdDetail>
-            <Button label="Contacter le propriétaire" width="300px"/>
-          </AdWrapper>
-        </AdContainer>
+          <Carousel 
+            showArrows={false}  // Masque les boutons "Précédent" et "Suivant"
+            showStatus={false}  // Masque le statut (ex : "1 sur 3")
+            swipeable={true}
+            useKeyboardArrows={true}
+            emulateTouch={true} 
+          >
+              <AdImg src="assets/images/ad/appartment.jpg" alt="Apartment" />
+              <AdImg src="assets/images/ad/appartment.jpg" alt="Apartment" />
+              <AdImg src="assets/images/ad/appartment.jpg" alt="Apartment" />
+            </Carousel>
+        <AdDetail>
+          <AdFeatures>
+              <AdFeaturesText fw="bold" fs="2em">1500€/mois</AdFeaturesText>
+              <AdFeaturesText>Appartement - Paris 16ème</AdFeaturesText>
+              <AdFeaturesText>5 pièces - 75m² - Ascenseur</AdFeaturesText>
+              <AdFeaturesText fw="bold" fs="1.2em">Caractéristiques</AdFeaturesText>
+              <AdFeaturesText>Jardin</AdFeaturesText>
+              <AdFeaturesText>Gardien</AdFeaturesText>
+              <AdFeaturesText fw="bold" fs="1.2em">Conditions de location</AdFeaturesText>
+              <AdFeaturesText>Minimum 6 mois</AdFeaturesText>
+              <AdFeaturesText fw="bold" fs="1.2em">Disponibilité</AdFeaturesText>
+              <AdFeaturesText>Immédiate</AdFeaturesText>
+              <AdFeaturesText>Note : 4.5/5</AdFeaturesText>
+          </AdFeatures>
+        </AdDetail>
+        <Button label="Contacter le propriétaire" width="300px"/>
+      </AdWrapper>
+    </AdContainer>
     </>
   )
 }
