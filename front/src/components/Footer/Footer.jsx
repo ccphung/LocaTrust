57651ee62049fe6d16bfe76a89999283e1909d91
MaryelN <maryel.navarromtz@gmail.com>
2024-12-21 10:31:11 +0100

    Creation de footer, icons et fonts

---
+++ front/src/components/Footer/Footer.jsx
@@ -0,0 +1,61 @@
+
+import React from 'react';
+import './Footer.css';
+
+
+export default function Footer() {
+  return <footer className="footer">
+    <div className="sb__footer section__padding">
+      <div className="sb__footer-links">
+        <div className='sb__footer-links-div'>
+          <h4>La société</h4>
+            <ul>
+              <li><a href="#">A propos de nous</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Presse</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+        </div>
+        <div className='sb__footer-links-div'>
+          <h4>La société</h4>
+            <ul>
+              <li><a href="#">A propos de nous</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Presse</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+        </div>
+        <div className='sb__footer-links-div'>
+          <h4>La société</h4>
+            <ul>
+              <li><a href="#">A propos de nous</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Presse</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+        </div>
+        <div className='sb__footer-links-div'>
+          <h4>Médias sociaux</h4>
+          <div className="socialmedia">
+            <p><img src="" alt="facebook icon"/></p>
+            <p><img src="" alt="instagram icon"/></p>
+            <p><img src="" alt="twitter icon"/></p>
+          </div>
+        </div> 
+        <div className='sb__footer-below'>
+          <div className="sb__footer-copywrite">
+            <p>© 2021 LocaTrust. Tous droits réservés.</p>
+        </div>   
+        <div className="sb__footer-below-links">
+          <ul>
+            <li><a href="#">Conditions générales</a></li>
+            <li><a href="#">Politique de confidentialité</a></li>
+            <li><a href="#">Plan du site</a></li>
+          </ul>  
+        </div>
+      </div> 
+    </div>   
+  </div>
+  </footer>
+
+}
--- front/src/pages/Contact.jsx
+++ front/src/pages/Contact.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer/Footer'
 
 function Contact() {
   return (
+    <>
+    <Nav />
     <div>Contact</div>
+    <Footer />
+
+  </>
   )
 }
 
